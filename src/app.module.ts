6a714010887104f14b9c89aa591d91a909ed1eed
zahramo <moosavizahra67@yahoo.com>
2020-12-16 18:42:03 +0330

    Change app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HelloModule } from './hello/hello.module';
-import BooksModule  from './books/books.module';
-import { UserModule } from './user/user.module';
-import  GenreModule  from './genre/genre.module';
+import { UserModule } from './User/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import UserEntity from './db/entity/user.entity';
+import BooksModule from './Books/books.module';
+import GenreModule from './Genre/genre.module';
+import BookEntity from './db/entity/book.entity';
+import GenreEntity from './db/entity/genre.entity';
 
 @Module({
-  imports: [HelloModule, BooksModule, UserModule, GenreModule],
+  imports: [UserModule ,
+            BooksModule,
+            GenreModule,
+    TypeOrmModule.forFeature(
+      [UserEntity, BookEntity , GenreEntity],
+    ),
+
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
